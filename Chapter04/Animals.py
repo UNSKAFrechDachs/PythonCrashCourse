b427b898f63afe557bbeb88a0c7c1d9afa2471b2
UNSKAFrechDachs <kevin.finkelmeyer@gmail.com>
2025-10-17 10:27:27 +0200

    Completed first exercise of Ch. 04

---
+++ Chapter04/Animals.py
@@ -0,0 +1,4 @@
+animals = ["mice", "rat", "rabbit"]
+for animal in animals:
+    print(f"A {animal} is a rodent")
+print("\nAll these animals are rodents!")
---
+++ Chapter04/pizza.py
@@ -0,0 +1,5 @@
+pizzas = ["Pepperoni", "Tuna", "Hawaii"]
+
+for pizza in pizzas:
+    print(f"I like {pizza} pizza")
+print("\nI really like pizza")
