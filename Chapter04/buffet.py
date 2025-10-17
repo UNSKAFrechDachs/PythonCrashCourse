0cf7ce65b7268064df25281b847a59b5c03eea83
UNSKAFrechDachs <kevin.finkelmeyer@gmail.com>
2025-10-17 11:07:42 +0200

    Completed fourth exercise of Ch. 04

---
+++ Chapter04/buffet.py
@@ -0,0 +1,8 @@
+menu = ("bacon and eggs", "coffee", "baked beans", "sausages", "salad")
+for item in menu:
+    print(item)
+#menu[0] = "olives"
+
+menu = ("olives", "orange juice", "baked beans", "sausages", "salad")
+for item in menu:
+    print(item)
