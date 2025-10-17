1a6c52d4b7ec92c1678cad14d3820b20f30b2b16
UNSKAFrechDachs <kevin.finkelmeyer@gmail.com>
2025-10-17 10:53:34 +0200

    Completed third exercise of Ch. 04

--- Chapter04/cubes.py
+++ Chapter04/cubes.py
@@ -1,3 +1,15 @@
 cubes = [value ** 3 for value in range(1,11)]
 for cube in cubes:
     print(cube)
+
+print("The first three items in the list are: ")
+for cube in cubes[:3]:
+    print(cube)
+
+print("Three items form the middle of the list are: ")
+for cube in cubes[2:5]:
+    print(cube)
+
+print("The last three items in the list are: ")
+for cube in cubes[-3:]:
+    print(cube)
--- Chapter04/pizza.py
+++ Chapter04/pizza.py
@@ -3,3 +3,16 @@ pizzas = ["Pepperoni", "Tuna", "Hawaii"]
 for pizza in pizzas:
     print(f"I like {pizza} pizza")
 print("\nI really like pizza")
+
+friend_pizzas = pizzas[:]
+
+pizzas.append("Diavolo")
+friend_pizzas.append("Sucuk")
+
+print("My favorite pizzas are:")
+for pizza in pizzas:
+    print(pizza)
+
+print("\nMy friend's favorite pizzas are:")
+for pizza in friend_pizzas:
+    print(pizza)
