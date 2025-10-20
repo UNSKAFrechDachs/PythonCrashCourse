2c13313c36f5e95ef5f8a1792d6c84ebf8be918a
UNSKAFrechDachs <kevin.finkelmeyer@gmail.com>
2025-10-20 20:47:10 +0200

    Completed fifth exercise of Chapter 08

---
+++ Chapter08/make_sandwiches.py
@@ -0,0 +1,4 @@
+def make_sandwich(*fillings):
+    print("Making sandwich with:")
+    for filling in fillings:
+        print(f"\t - {filling}")
---
+++ Chapter08/sandwiches.py
@@ -0,0 +1,4 @@
+import make_sandwiches as ms
+ms.make_sandwich("Tuna", "Mayo", "Corn")
+ms.make_sandwich("Ham", "Salad")
+ms.make_sandwich("Cheese")
