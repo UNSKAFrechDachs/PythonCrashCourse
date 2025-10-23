c84c4d91c409948ba41d25be1da912d7c80ebbb1
UNSKAFrechDachs <kevin.finkelmeyer@gmail.com>
2025-10-23 19:38:00 +0200

    Completed third exercise of Chapter 09

--- Chapter09/restaurant.py
+++ Chapter09/restaurant.py
@@ -1,7 +1,8 @@
-class Restaurant():
+class Restaurant:
     def __init__(self, restaurant_name, cuisine_type):
         self.restaurant_name = restaurant_name
         self.cuisine_type = cuisine_type
+        self.number_served = 0
 
     def describe_restaurant(self):
         print(f"The restaurant {self.restaurant_name} serves {self.cuisine_type}.")
@@ -9,13 +10,38 @@ class Restaurant():
     def open_restaurant(self):
         print(f"The restaurant {self.restaurant_name} has been opened.")
 
+    def set_number_served(self, number_served):
+        self.number_served = number_served
+
+    def increment_number_served(self, number_served):
+        self.number_served += number_served
+
+
+class IceCreamStand(Restaurant):
+    def __init__(self, restaurant_name, cuisine_type, flavors = ["Vanilla"]):
+        super().__init__(restaurant_name, cuisine_type)
+        self.flavors = flavors
+
+    def list_flavors(self):
+        print("These flavors are available:")
+        for flavor in self.flavors:
+            print(f"\t- {flavor}")
+
+
 first_restaurant = Restaurant("Daikan", "japanese food")
+second_restaurant = Restaurant("MacDo", "fast food")
+third_restaurant = IceCreamStand("Fontanella", "icecream", ["Mango", "Strawberry", "Mint-Choco"])
+
 print(first_restaurant.restaurant_name)
 print(first_restaurant.cuisine_type)
 first_restaurant.describe_restaurant()
 first_restaurant.open_restaurant()
-
-second_restaurant = Restaurant("MacDo", "fast food")
-third_restaurant = Restaurant("Fontanella", "icecream")
 second_restaurant.describe_restaurant()
 third_restaurant.describe_restaurant()
+print(third_restaurant.number_served)
+third_restaurant.set_number_served(3)
+print(third_restaurant.number_served)
+third_restaurant.increment_number_served(3)
+print(third_restaurant.number_served)
+third_restaurant.list_flavors()
+
--- Chapter09/user.py
+++ Chapter09/user.py
@@ -1,4 +1,4 @@
-class User():
+class User:
     """Simple user class."""
     def __init__(self, first_name, last_name, favorite_color, birth_date, **additional_info):
         self.first_name = first_name
@@ -6,6 +6,7 @@ class User():
         self.favorite_color = favorite_color
         self.birth_date = birth_date
         self.additional_info = additional_info
+        self.login_attempts = 0
 
     def describe_user(self):
         print(f"""This is {self.first_name.title()} {self.last_name.title()}.
@@ -15,6 +16,27 @@ Here's some additional information: {self.additional_info}.""")
     def greet_user(self):
         print(f"Hello {self.first_name.title()} {self.last_name.title()}. Nice to see you!")
 
+    def increment_login_attempts(self):
+        self.login_attempts += 1
+
+    def reset_login_attempts(self):
+        self.login_attempts = 0
+
+
+class Admin(User):
+    """Admin class."""
+    def __init__(self, first_name, last_name, favorite_color, birth_date, privileges, **additional_info):
+        User.__init__(self, first_name, last_name, favorite_color, birth_date, **additional_info)
+        self.privileges = Privileges(privileges)
+
+class Privileges:
+    """Class Privileges."""
+    def __init__(self, privileges):
+        self.privileges = privileges
+    def show_privileges(self):
+        print("Available privileges:")
+        for privilege in self.privileges:
+            print(f"\t- {privilege}")
 
 my_user = User("Max",
                "Mustermann",
@@ -22,10 +44,11 @@ my_user = User("Max",
                "11.09.2002",
                gender="male",
                eye_color="blue")
-other_user = User("Marion",
+other_user = Admin("Marion",
                   "Musterfrau",
                   "fuchsia",
                   "1.1.1980",
+                   privileges=["Can ban"],
                   gender="female",
                   eye_color="blue",
                   hair_color="red",
@@ -34,3 +57,11 @@ my_user.describe_user()
 my_user.greet_user()
 other_user.describe_user()
 other_user.greet_user()
+
+
+for _ in range(15):
+    my_user.increment_login_attempts()
+    print(my_user.login_attempts)
+my_user.reset_login_attempts()
+print(my_user.login_attempts)
+other_user.privileges.show_privileges()
